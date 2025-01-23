648cfd6ee36a8215db4093bc1f49de366171661b
schmeb0704 <seanauditor0704@gmail.com>
2025-01-24 04:50:55 +0800

    fix: import toastify to index

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import "@mdi/font/css/materialdesignicons.css"
+import 'vue3-toastify/dist/index.css'
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'

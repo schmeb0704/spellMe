18e0ad813ec1b97a62430a965483f886e24cb0d7
schmeb0704 <seanauditor0704@gmail.com>
2025-01-27 01:16:36 +0800

    feat: create level menu dropdown

---
+++ src/components/LevelDropdown.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import { ref } from 'vue'
+  import { useDifficultyStore } from '@/stores/difficulty';
+
+  const difficultyStore = useDifficultyStore()
+  
+  
+</script>
+
+<template>
+  <v-container>
+    <v-select
+      v-model="difficultyStore.selectedOption"
+      :items="difficultyStore.options"
+      label="Select Difficulty"
+      item-title="label"
+      item-value="value"
+    >
+    </v-select>
+  </v-container>
+</template>
+
+<style scoped>
+  .w-full {
+    width: 100%;
+  }
+</style>
